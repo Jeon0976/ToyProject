8e87df34abed17fb5df58ce703062156d77beff5
J-S-Hun <jsh097610@gamil.com>
2023-02-02 16:37:53 +0900

    keywordNews pod file 추가

---
+++ KeywordNews/KeywordNews/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  KeywordNews
+//
+//  Created by 전성훈 on 2023/02/02.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window?.windowScene = windowScene
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+    }
+
+}
+
---
+++ KeywordNews/KeywordNews/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  KeywordNews
+//
+//  Created by 전성훈 on 2023/02/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
