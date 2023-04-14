6a12e14bb0b1cedc6f8d8b2873737b039251e04b
Jeon Seoung Hun <jsh097610@gmail.com>
2023-04-14 14:59:05 +0900

    RxSwiftFinish4H start

---
+++ RxSwiftFinish4H/season2/step1/RxSwift+MVVM/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  RxSwift+MVVM
+//
+//  Created by iamchiwon on 05/08/2019.
+//  Copyright © 2019 iamchiwon. All rights reserved.
+//
+
+import RxSwift
+import SwiftyJSON
+import UIKit
+
+// 최초 Load 버튼 눌렀을 때 JSon을 다운 받고 있어서 인티게이터가 안보이고 시간이 멈춤
+// 비동기로 돌려야 함
+
+let MEMBER_LIST_URL = "https://my.api.mockaroo.com/members_with_avatar.json?key=44ce18f0"
+
+class ViewController: UIViewController {
+    @IBOutlet var timerLabel: UILabel!
+    @IBOutlet var editView: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
+            self?.timerLabel.text = "\(Date().timeIntervalSince1970)"
+        }
+    }
+
+    private func setVisibleWithAnimation(_ v: UIView?, _ s: Bool) {
+        guard let v = v else { return }
+        UIView.animate(withDuration: 0.3, animations: { [weak v] in
+            v?.isHidden = !s
+        }, completion: { [weak self] _ in
+            self?.view.layoutIfNeeded()
+        })
+    }
+
+    
+    
+    // MARK: SYNC
+
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+
+    @IBAction func onLoad() {
+        editView.text = ""
+
+        self.setVisibleWithAnimation(self.activityIndicator, true)
+
+        
+    // 비동기 작업을 위한 dispatchQueue
+        DispatchQueue.global().async {
+
+            let url = URL(string: MEMBER_LIST_URL)!
+            let data = try! Data(contentsOf: url)
+            let json = String(data: data, encoding: .utf8)
+            
+            DispatchQueue.main.async {
+                self.editView.text = json
+                self.setVisibleWithAnimation(self.activityIndicator, false)
+            }
+        }
+        
+    }
+}
