2529bd4a2eede19317f6776720f0280f8c64df49
Jeon Seoung Hun <jsh097610@gmail.com>
2023-06-03 17:57:15 +0900

    SweetSwiftUI 2장 완료

---
+++ SweetSwiftUI/FruitMart/Product.swift
@@ -0,0 +1,17 @@
+//
+//  Product.swift
+//  FruitMart
+//
+//  Created by 전성훈 on 2023/06/03.
+//  Copyright © 2023 Giftbot. All rights reserved.
+//
+
+import Foundation
+
+struct Product: Decodable {
+    let name: String
+    let imageName: String
+    let price: Int
+    let description: String
+    var isFavorite: Bool = false
+}
--- SweetSwiftUI/FruitMart/Source/User Interface/Home/Home.swift
+++ SweetSwiftUI/FruitMart/Source/User Interface/Home/Home.swift
@@ -9,10 +9,33 @@
 import SwiftUI
 
 struct Home: View {
-  var body: some View {
-    Text("Hello, World!")
-  }
+    @State private var products: [Product]?
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                ForEach(products ?? [], id: \.name) { product in
+                    ProductRow(product: product)
+                }
+            }
+            .onAppear {
+                products = loadProducts()
+            }
+        }
+    }
 }
+
+// JSON file Decoder
+private extension Home {
+    func loadProducts() -> [Product]? {
+        guard let url = Bundle.main.url(forResource: "ProductData", withExtension: "json"),
+              let data = try? Data(contentsOf: url) else { return nil }
+        
+        let decoder = JSONDecoder()
+        return try? decoder.decode([Product].self, from: data)
+    }
+}
+
 
 struct Home_Previews: PreviewProvider {
   static var previews: some View {
